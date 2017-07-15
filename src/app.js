9c003dabc9e06da5123d5970aaec10ae5cf64a86
Xinyang <xinyzhang9@gmail.com>
2017-07-15 10:16:44 +0800

    linear scale

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-document.body.innerHTML  = d3.version
+var linearScale = d3.scaleLinear()
+  .domain([0,100])
+  .range([0,600])
+  .clamp(true);
+
+console.log(linearScale(-100));
+console.log(linearScale(630));
+
+console.log(linearScale.invert(300));
